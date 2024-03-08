81e354362a020891c64e591808219cdf4951f0cd
anvimaa <70594573+anvimaa@users.noreply.github.com>
2024-03-09 00:33:27 +0100

    sds

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(express.json());
 
