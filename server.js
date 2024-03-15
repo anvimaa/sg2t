1368cadea9fd28b979fbed4c40dbae59a9bc2a48
anvimaa <70594573+anvimaa@users.noreply.github.com>
2024-03-15 18:21:14 +0100

    18

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ hbs.registerPartials(__dirname + "/views/parcials");
 app.use("/", routes);
 
 app.listen(port, () => {
-  console.log(`Server running http://localhost:${port}/`);
+  console.log(`Server runnin http://localhost:${port}/`);
 });
