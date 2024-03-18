f5fa12b9fa27c5c439ff6850d42871de10c3ea3b
anvimaa <anvimaa@gmail.com>
2024-03-18 16:09:56 +0100

    let a

--- routes/settings.js
+++ routes/settings.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const sqlite3 = require("better-sqlite3");
 
+let a = 2;
 const router = express.Router();
 const prisma = require("../db");
 const { isAdmin } = require("./midlewares");
